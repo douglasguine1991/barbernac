8c8567aeb9c8d549dba776e0f53f0a01c4489b4e
Douglas <douglassamuelg@email.com>
2025-06-13 11:58:40 -0300

    BIG

--- app/models/Depoimento.php
+++ app/models/Depoimento.php
@@ -6,15 +6,15 @@ class Depoimento extends Model
     public function getTodosDepoimentos()
     {
         $sql = "SELECT 
-            c.nome AS nome_cliente, 
-            c.telefone AS telefone_cliente, 
-            e.nome_uf AS nome_uf,
+            c.nome AS nome, 
+            c.telefone AS telefone, 
+            e.id_uf AS id_uf,
             e.sigla_uf AS sigla_uf, 
-            d.comentario AS descricao_depoimento, 
+            d.comentario AS comentario, 
             d.data_depoimento, 
             d.nota, 
             c.foto_cliente, 
-            c.nome AS alt_foto_cliente
+            c.nome AS foto_cliente
         FROM depoimento d
         INNER JOIN clientes c ON d.cliente_id = c.id
         LEFT JOIN estado e ON c.id_uf = e.id_uf
@@ -28,16 +28,6 @@ class Depoimento extends Model
         }
     
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-    
-
-    // Conta quantos depoimentos têm nota 4 ou 5
-    public function getDepoimentoPositivo()
-    {
-        $sql = "SELECT COUNT(*) AS qtde_depoimento FROM depoimento WHERE nota >= 4";
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     // Adiciona um novo depoimento
