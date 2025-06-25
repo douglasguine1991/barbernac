f4c91b2ec676112b04d8f244a7026356f10ec9e4
vytorminzoon <vytorminzonsenac@gmail.com>
2025-06-25 11:59:28 -0300

    qrcode adicionado

--- app/views/template/contato.php
+++ app/views/template/contato.php
@@ -5,46 +5,28 @@
     <div class="row">
       <div class="col-md-12 text-center">
         <div class="title mb-5">
-          <h2>Solicite um <span >Retorno</span></h2>
+          <h2>Acesse o <span>Aplicativo</span></h2>
         </div>
       </div>
     </div>
 
     <!-- Formulário e imagem -->
     <div class="row align-items-center">
-      <!-- Formulário -->
+      <!-- Coluna do QR Code -->
       <div class="col-lg-6">
-        <form class="main_form">
-          <div class="row g-3">
-            <div class="col-12">
-              <input class="form-control" placeholder="Seu Nome" type="text" name="Your Name" />
-            </div>
-            <div class="col-12">
-              <input class="form-control" placeholder="Telefone" type="text" name="Phone" />
-            </div>
-            <div class="col-12">
-              <input class="form-control" placeholder="Email" type="email" name="Email" />
-            </div>
-            <div class="col-12">
-              <input class="form-control" placeholder="Data" type="date" name="Date" />
-            </div>
-            <div class="col-12">
-              <textarea class="form-control textarea" placeholder="Mensagem" name="Message"></textarea>
-            </div>
-            <div class="col-12 text-end">
-              <button class="send">Enviar</button>
-            </div>
+        <div class="d-flex justify-content-center">
+          <div style="background-color: #2a2a2a; width: 300px; height: 300px; display: flex; align-items: center; justify-content: center; border-radius: 8px;">
+            <img src="<?= BASE_URL ?>assets/img/qrcode.png" alt="QR Code" style="max-width: 300px; max-height: 300px;">
           </div>
-        </form>
+        </div>
       </div>
 
-      <!-- Imagem -->
+
+
+      <!-- Coluna do lado da imagem do qr code -->
       <div class="col-lg-6 mt-4 mt-lg-0">
-        <div class="img-box text-center ratio ratio-4x3">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!4v1747430359397!6m8!1m7!1sCAoSF0NJSE0wb2dLRUlDQWdJRFdrNk91LUFF!2m2!1d-23.49557952787197!2d-46.43183217791507!3f293.9116823592974!4f-61.36619222538435!5f0.5970117501821992"
-            allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-          </iframe>
+        <div class="img-box text-center ratio ratio-4x3" style="background-color:#2a2a2a ">
+        <h4>AGENDE SEU HORÁRIO</h4>
         </div>
       </div>
     </div>
